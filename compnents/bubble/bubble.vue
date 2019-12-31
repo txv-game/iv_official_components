88edc1d5ee7aa2ba6ed3f785b5c66e81a031b75f
ljx <1044680698@qq.com>
2019-12-31 14:55:33 +0800

    feat:气泡/滑动解锁组件

---
+++ compnents/bubble/bubble.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="bubble"
+    @click="onClick($event)"
+    v-show="visible"
+    :style="{
+        top: y + '%',
+        left: x + '%',
+        width: width + '%',
+        height: height + '%'
+    }">
+    <span class="bubble-text">{{text}}</span>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            selected: false
+        };
+    },
+
+    mounted() {
+        this.bridge.on('videoTimeUpdate', time => {
+            if (this.selected) return; 
+            if (time >= this.startTime && time <= this.endTime) {
+                this.show();
+                if (this.stop) {
+                    vBridge.pause();
+                }
+            } else {
+                this.hide();
+            }
+        })
+    },
+
+    methods: {
+        onClick(e) {
+            this.bridge.invoke('action', this.action);
+            this.selected = true;
+            this.hide();
+            if (this.stop) {
+                vBridge.play();
+            }
+        }
+    }
+};
+</script>
+<style>
+.bubble {
+    position: absolute;
+    display: table;
+    border-radius: 25px;
+    background-color: rgba(0, 0, 0, 0.5);
+    border: 1px solid white;
+    text-align: center;
+}
+
+.bubble .bubble-text {
+    display: table-cell;
+    vertical-align: middle;
+}
+</style>
---
+++ compnents/slide-lock/slide-lock.vue
@@ -0,0 +1,128 @@
+<template>
+<div class="container"
+    v-show="visible"
+    :style="{
+        top: y + '%',
+        left: x + '%',
+        width: width + '%',
+        height: height + '%'
+    }">
+    <div class="track-bg" :style="{
+            transform: `rotate(${deg}deg)`
+        }">
+        <img ref="trackBg" :src="bg" v-show="visible" :style="{
+            width: 100 * ratio + '%'
+        }">
+    </div>
+    <div class="smooth"
+        :style="{
+            transform: 'translate(' + left + 'px,' + top + 'px)'
+        }">
+        <div class="block" v-drag="{
+                dragmove: onDragMove,
+                dragend: onDragEnd
+            }">
+            <img ref="blockBg" class="block-bg" :src="blockBg" :style="{
+                transform: `rotate(${deg}deg)`
+            }">
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            left: 0,
+            top: 0,
+
+            ratio: 1,
+        };
+    },
+    mounted() {
+        this.bridge.on('videoTimeUpdate', time => {
+            if (!this.loaded && time >= this.startTime && time <= this.endTime) {
+                this.show();
+                vBridge.pause();
+            }
+        });
+        let img = new Image();
+        img.src = this.bg;
+        img.onload = () => {
+            this.ratio = img.width / img.height;
+        };
+    },
+    methods: {
+        onDragMove(pos) {
+            let deg = this.deg / 180 * Math.PI;
+            let left = pos.left + this.left;
+            let top = Math.tan(deg) * left;
+            let distance = this.$refs.trackBg.width - this.$refs.blockBg.width;
+            if (!(distance > 0)) {
+                distance = Infinity;
+            }
+            let targetX = distance * Math.cos(deg);
+            if (targetX * left < 0) {
+                left = top = 0;
+            } else if (Math.abs(left) > Math.abs(targetX)) {
+                left = targetX;
+                top = distance * Math.sin(deg);
+            }
+            return {
+                left: left - this.left,
+                top: top - this.top
+            };
+        },
+        onDragEnd(pos) {
+            let deg = this.deg / 180 * Math.PI;
+            let left = pos.left + this.left;
+            let top = Math.tan(deg) * left;
+            let distance = this.$refs.trackBg.width - this.$refs.blockBg.width;
+            if (!(distance > 0)) {
+                distance = Infinity;
+            }
+            if (Math.abs(Math.sqrt(left * left + top * top) - distance) < 10) {
+                this.bridge.invoke('action', this.action);
+                this.hide();
+                vBridge.play();
+                this.loaded = true;
+            } else {
+                this.left = -pos.left;
+                this.top = -pos.top;
+            }
+        }
+    }
+};
+</script>
+<style>
+.container {
+    position: absolute;
+}
+.track-bg {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+}
+.track-bg img{
+    height: 100%;
+    max-width: none;
+    transform: translateZ(0);
+}
+.block {
+    width: 100%;
+    height: 100%;
+}
+.block-bg {
+    pointer-events: none;
+    width: 100%;
+    height: 100%;
+}
+.smooth {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 99;
+    transition: transform .2s ease;
+}
+</style>
