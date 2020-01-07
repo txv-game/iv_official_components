5ed03c561c037816e18f7dc48a64823ade7dbf4a
ljx <1044680698@qq.com>
2020-01-07 12:58:29 +0800

    feat:循环播放控制

---
+++ compnents/bubble-types/bubble-types.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="bubble"
+    @click="onClick($event)"
+    v-show="visible"
+    :style="{
+        top: y + '%',
+        left: x + '%',
+        width: width + '%',
+        height: height + '%'
+    }">
+    <span class="bubble-text">{{text}}</span>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            selected: false
+        };
+    },
+
+    mounted() {
+        this.bridge.on('videoTimeUpdate', time => {
+            if (this.selected) return;
+
+            let handleMethod = `handle${this.playType}`;
+            if (typeof this[handleMethod] === 'function') {
+                this[handleMethod](time);
+            }
+        });
+    },
+
+    methods: {
+        onClick(e) {
+            this.bridge.invoke('action', this.action);
+            this.selected = true;
+            this.hide();
+            if (this.playType === 'stop') {
+                vBridge.play();
+            }
+        },
+        // 单次播放模式
+        handleOnce(time) {
+            this[time >= this.startTime && time <= this.endTime ? 'show' : 'hide']
+        },
+        // 视频暂停模式
+        handleStop(time) {
+            if (time >= this.startTime && time <= this.endTime) {
+                this.show();
+                vBridge.pause();
+            } else {
+                this.hide();
+            }
+        },
+        // 循环播放模式
+        handleLoop(time) {
+            if(time > this.endTime) {
+                this.bridge.invoke('seek', this.startTime);
+            }
+            this.show();
+        },
+    }
+};
+</script>
+<style>
+.bubble {
+    position: absolute;
+    display: table;
+    border-radius: 25px;
+    background-color: rgba(0, 0, 0, 0.5);
+    border: 1px solid white;
+    text-align: center;
+}
+
+.bubble .bubble-text {
+    display: table-cell;
+    vertical-align: middle;
+}
+</style>
---
+++ compnents/bubble-types/readme.md
@@ -0,0 +1,13 @@
+# 气泡-多种播放控制
+
+> version 1.0.0
+
+## 预览效果(循环播放)
+<div align="center">
+  <img width="50%" src="https://puui.qpic.cn/vupload/0/1578372893093_bjm8r6d93cv.gif/0">
+</div>
+
+## 表单配置
+<div align="center">
+  <img width="50%" src="https://puui.qpic.cn/vupload/0/1578373041404_1oml7te2slc.png/0">
+</div>
